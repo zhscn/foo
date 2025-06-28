164285f8ed5a353e8b882c030f3cf959390be23e
zs <zs@ijk.dev>
2025-06-28 20:02:45 +0800

    add absl flag example

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,32 @@
 #include "version.hh"
 
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+#include <absl/flags/usage.h>
+#include <absl/flags/usage_config.h>
+#include <absl/strings/match.h>
 #include <fmt/format.h>
+
+void handle_args(int argc, char** argv) {
+  absl::FlagsUsageConfig cfg;
+  cfg.version_string = []() -> std::string { return "0.1.0\n"; };
+  cfg.contains_help_flags = [](absl::string_view sv) -> bool {
+    return !absl::StrContains(sv, "absl");
+  };
+  cfg.normalize_filename = [](absl::string_view sv) -> std::string {
+    size_t len = 0;
+    if (auto n = sv.rfind("absl/"); n != absl::string_view::npos) {
+      len = n;
+    } else if (auto n = sv.rfind("root/"); n != absl::string_view::npos) {
+      len = n;
+    }
+    return {sv.data() + len, sv.length() - len};
+  };
+
+  absl::SetFlagsUsageConfig(cfg);
+  absl::SetProgramUsageMessage("usage");
+  absl::ParseCommandLine(argc, argv);
+}
 
 int main() {
   fmt::println("{}\n{}\n{}", get_project_name(), get_project_version(),
