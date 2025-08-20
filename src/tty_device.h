c8c651899df68a3cec8c78d691e9c7a1fd61a901
zs <zhangsong325@gmail.com>
2025-08-21 00:05:32 +0800

    add tty_device library

---
+++ src/tty_device.h
@@ -0,0 +1,24 @@
+#ifndef TTY_DEVICE_H__
+#define TTY_DEVICE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void *tty_device_t;
+
+tty_device_t tty_open();
+void tty_close(tty_device_t td);
+int tty_enable_raw(tty_device_t td);
+int tty_disable_raw(tty_device_t td);
+int tty_readable(tty_device_t td, int ms);
+int tty_read(tty_device_t td, char *buf, int len);
+int tty_write(tty_device_t td, char *buf, int len);
+int tty_is_backspace(tty_device_t td, char c);
+int tty_get_window_size(tty_device_t td, int *row, int *col);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
